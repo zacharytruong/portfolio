ca874a15f8a1743555a042673fc251960abd5c7d
Zachary Truong <zacharytruong@gmail.com>
2021-10-23 18:06:19 -0500

    working on functions

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -1,15 +1,18 @@
 // Global variable
 let previousNumber;
+let currentNumber;
+let currentOperator;
 const history = document.querySelector('.history');
 const buttons = document.querySelectorAll('.button');
 
 // Global functions        
 function displayHistory(value){
-   if (previousNumber === undefined){
+   if (previousNumber === undefined ||
+      Number.isNaN(previousNumber)){
      history.innerText = '';
    } else history.innerText = previousNumber;
 }
-function addition(a, b){
+function add(a, b){
   return a + b;
 }
 function subtract(a, b){
@@ -28,3 +31,30 @@ function operate(callBackFn, a, b){
 }
 
 // Events for buttons
+buttons.forEach(button => {
+  button.addEventListener('click', e => {
+    if (e.target.value === 'add' ||
+        e.target.value === 'subtract'||
+        e.target.value === 'multiply' ||
+        e.target.value === 'divide') {
+      return currentOperator = e.target.value;
+    } else if (e.target.value === 'equal'){
+        let result = window[currentOperator](previousNumber, currentNumber);
+        currentNumber = result;
+        displayHistory(currentNumber);
+        return currentNumber
+    } else if (!isNaN(e.target.value && currentOperator === undefined)){
+        previousNumber = parseInt(e.target.value);
+        displayHistory(previousNumber);
+        return previousNumber;
+    } else {
+        currentNumber = parseInt(e.target.value);
+        displayHistory(currentNumber)
+        return currentNumber;
+    }
+  })
+})
+
+console.log(previousNumber)
+console.log(currentNumber)
+console.log(currentOperator)
